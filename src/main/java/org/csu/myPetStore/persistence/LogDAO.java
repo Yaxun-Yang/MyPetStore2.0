8de3670570b40c07ffc6bee5d72b5553bc746bef
YaxunYang <44467262+Yaxun-Yang@users.noreply.github.com>
2020-03-27 21:38:10 +0800

    del

--- src/main/java/org/csu/myPetStore/persistence/LogDAO.java
+++
@@ -1,6 +0,0 @@
-package org.csu.myPetStore.persistence;
-
-public interface LogDAO {
-    void insertNewLog(String username,String logType, String objectId);
-
-}
--- src/main/java/org/csu/myPetStore/persistence/OrderStatusDAO.java
+++
@@ -1,9 +0,0 @@
-package org.csu.myPetStore.persistence;
-
-
-import org.csu.myPetStore.domain.Order;
-
-public interface OrderStatusDAO {
-   void  CreateNewOrder(Order order);
-   void  OrderPaid(Order order);
-}
